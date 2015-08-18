20da33f2d16b8a56864c19d4404c71a18db9289c
zack Frazier <zfrazier@gmail.com>
2015-08-18 17:45:32 -0500

    roughing in deps

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/*global -$ */
+'use strict';
+var pkg = require('./package.json');
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+
+var server;
+
+gulp.task('default', function () {
+
+});
