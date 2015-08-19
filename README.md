690e06958804b91549c97336d68a6e1d616eaf9c
zack Frazier <zfrazier@gmail.com>
2015-08-19 09:36:42 -0500

    app scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # strong-css
+
+## ITCSS
+
+playing around with ITCSS
+
+```
+_____________
+\___________/   settings
+ \_________/    tools
+  \_______/     generic
+   \_____/      elements
+    \___/       objects
+     \_/        components
+      V         trumps
+
+```
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,46 @@
 /*global -$ */
 'use strict';
-var pkg = require('./package.json');
+var json__pkg = require('./package.json');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
+var dir__dist = 'dist';
+var dir__www = 'www';
 
 var server;
 
-gulp.task('default', function () {
 
+gulp.task('serve', ['www'], function () {
+  server = browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: [dir__www],
+      routes: {
+        '/bower_components': 'bower_components',
+        '/css': 'css'
+      }
+    }
+  });
+  gulp.watch('src/*.html', ['www']);
 });
+
+
+
+gulp.task('www', function () {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest(dir__www))
+    .pipe(reload());
+});
+
+
+gulp.task('default', function () {
+  gulp.start('serve');
+});
+
+function reload() {
+  if (server) {
+    return browserSync.reload({ stream: true });
+  }
+  return $.util.noop();
+}
