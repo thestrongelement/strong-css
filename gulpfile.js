443f9c36793c57736b73c9bae3fb257152fe53ea
zack Frazier <zfrazier@gmail.com>
2015-08-23 18:58:01 -0500

    reorg, begins css comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,21 @@
 /*global -$ */
 'use strict';
-
-var json__pkg = require('./package.json');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
+var del = require('del');
+
+var json__pkg = require('./package.json');
+var cli_argv = require('minimist')(process.argv.slice(2));
+var minify = typeof cli_argv.minify != 'undefined';
+
 var dir__public = 'public';
 var dir__src_html = 'views';
 var dir__src_css = 'css';
 var dir__dist = 'dist';
 var dir__www = 'www';
 
-var cli_argv = require('minimist')(process.argv.slice(2));
-var minify = typeof cli_argv.minify != 'undefined';
-
 var server;
-
-console.log(dir__src_html+'/**/*.html')
-console.log(dir__src_css+'/**/*')
-
 
 gulp.task('serve', ['www'], function () {
   server = browserSync({
@@ -32,12 +29,12 @@ gulp.task('serve', ['www'], function () {
       }
     }
   });
-  gulp.watch(dir__src_html+'/**/*.html', ['www']);
+  gulp.watch(dir__src_html+'/**/*.html', ['html']);
   gulp.watch(dir__src_css+'/**/*', ['css']);
   gulp.watch(dir__public+'/**/*', ['public']);
 });
 
-gulp.task('www', ['css','public'], function () {
+gulp.task('html', function () {
   return gulp.src(dir__src_html+'/**/*.html')
     .pipe(gulp.dest(dir__www))
     .pipe(reload());
@@ -58,7 +55,6 @@ gulp.task('css', function () {
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest(dir__www + '/css'))
     .pipe(reload());
-    console.log(dir__src_css)
 });
 
 // process static files
@@ -67,10 +63,14 @@ gulp.task('public', function() {
 	  .pipe(gulp.dest(dir__www));
 });
 
+//build
+gulp.task('www', $.sequence('clean',['css','public'],'html'));
+
 gulp.task('default', function () {
   gulp.start('serve');
-  console.log('minify = ' + minify)
 });
+
+gulp.task('clean', del.bind(null, ['.tmp', dir__www]));
 
 function reload() {
   if (server) {
