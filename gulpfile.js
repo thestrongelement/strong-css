8c1dba9e4927649a82c655f598e8604f881d35b3
zack Frazier <zfrazier@gmail.com>
2015-08-22 19:14:17 -0500

    ongoing setup

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var json__pkg = require('./package.json');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
+var dir__src = 'src';
 var dir__dist = 'dist';
 var dir__www = 'www';
 
@@ -22,17 +23,37 @@ gulp.task('serve', ['www'], function () {
       }
     }
   });
-  gulp.watch('src/*.html', ['www']);
+  gulp.watch(dir__src+'/**/*.html', ['www']);
 });
 
-
-
 gulp.task('www', function () {
-  return gulp.src('src/*.html')
+  return gulp.src(dir__src+'/**/*.html')
     .pipe(gulp.dest(dir__www))
     .pipe(reload());
 });
 
+gulp.task('css', function () {
+  return gulp.src('src/css/*.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      outputStyle: 'nested', // libsass doesn't support expanded yet
+      precision: 10,
+      includePaths: ['.'],
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    .pipe($.postcss([
+      require('autoprefixer-core')({browsers: ['last 1 version']})
+    ]))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest(dir__www + '/css'))
+    .pipe(reload());
+});
+
+// process static files
+gulp.task('public', function() {
+  return gulp.src('public/**/*')
+	  .pipe(gulp.dest(dir__www));
+});
 
 gulp.task('default', function () {
   gulp.start('serve');
