afe8b2c501056a668ffcbee18aae490e868ead53
zack Frazier <zfrazier@gmail.com>
2016-03-24 07:51:21 -0500

    starts cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('serve', ['www'], function () {
     port: 9000,
     server: {
       baseDir: [dir__www],
-      index: 'strong.html',
+      index: 'index.html',
       routes: {
         '/bower_components': 'bower_components',
         '/css': 'css'
